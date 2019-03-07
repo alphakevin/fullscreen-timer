9da96ddc5aa5eb3eb76eaf7bd8cd9fd25ce3fefe
alphakevin <get.so.smart@gmail.com>
2019-03-07 12:31:22 +0800

    Updates

---
+++ precache-manifest.5b897f82e61531d17ba6a50d38c9bf42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e88e5a3993a1331d4313",
+    "url": "/static/js/main.e88e5a39.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "e88e5a3993a1331d4313",
+    "url": "/static/css/main.8f200aeb.chunk.css"
+  },
+  {
+    "revision": "289e4dda494bfb47ab685c400234a4f2",
+    "url": "/index.html"
+  }
+];
