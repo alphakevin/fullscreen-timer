6529f51b0335a5a08730fdf2f26db8474619fd4f
alphakevin <get.so.smart@gmail.com>
2019-08-09 08:29:50 +0000

    Updates

---
+++ precache-manifest.d8e1170d828631e101870ea4dde3401f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eabb17bc67a77cf32e74",
+    "url": "/fullscreen-timer/static/js/runtime~main.eabb17bc.js"
+  },
+  {
+    "revision": "da4d1f091970a21fba3b",
+    "url": "/fullscreen-timer/static/js/main.da4d1f09.chunk.js"
+  },
+  {
+    "revision": "8a8cd1dc674c7720e55f",
+    "url": "/fullscreen-timer/static/js/2.8a8cd1dc.chunk.js"
+  },
+  {
+    "revision": "da4d1f091970a21fba3b",
+    "url": "/fullscreen-timer/static/css/main.60d873f4.chunk.css"
+  },
+  {
+    "revision": "4f21ec04e4a058a482cf27a8e35ecc35",
+    "url": "/fullscreen-timer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fullscreen-timer/precache-manifest.b7e10842b07eb3991f2392197af0cda5.js"
+  "/fullscreen-timer/precache-manifest.d8e1170d828631e101870ea4dde3401f.js"
 );
 
 workbox.clientsClaim();
